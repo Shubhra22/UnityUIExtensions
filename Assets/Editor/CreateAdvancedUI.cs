c8b0bc848c70cdfdcfbaadc8dadefa4df57596d3
Shubhra <ssuvro22@outlook.com>
2020-12-11 18:58:04 -0800

    Custom Slider with Scale Effect

---
+++ Assets/Editor/CreateAdvancedUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AdvancedScrollRect))]
+public class CreateAdvancedUI:UnityEditor.UI.ScrollRectEditor
+{
+   public override void OnInspectorGUI() {
+
+      AdvancedScrollRect component = (AdvancedScrollRect)target;
+
+      base.OnInspectorGUI();
+      component.selectionPoint =
+         (Transform)EditorGUILayout.ObjectField("TargetPoint", component.selectionPoint, typeof(Transform));
+      component.buttonScale = EditorGUILayout.Slider("ScaleValue", component.buttonScale, 1, 4);
+   }
+}
---
+++ Assets/Scripts/AdvancedScrollRect.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class AdvancedScrollRect : ScrollRect
+{
+    private GraphicRaycaster m_Raycaster;
+    PointerEventData m_PointerEventData;
+    EventSystem m_EventSystem;
+
+    public Transform selectionPoint;
+    public float buttonScale;
+    
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        
+        m_Raycaster = FindObjectOfType<GraphicRaycaster>();
+        m_EventSystem = FindObjectOfType<EventSystem>();
+        m_PointerEventData = new PointerEventData(m_EventSystem);
+        m_PointerEventData.position = selectionPoint.position;
+    }
+
+    private void Update()
+    {
+        foreach (Transform child in content)
+        {
+            if (OnEntered(child))
+            {
+                child.DOScale(Vector3.one * buttonScale, 0.4f);
+            }
+            else
+            {
+                child.DOScale(Vector3.one, 0.4f);
+            }
+        }
+    }
+
+    private bool OnEntered(Transform obj)
+    {
+        List<RaycastResult> _results = new List<RaycastResult>();
+        m_Raycaster.Raycast(m_PointerEventData, _results);
+        if (_results.Count > 0)
+        {
+            if (_results[0].gameObject == obj.gameObject)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+}
