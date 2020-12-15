a687ae8fc5d5163f1635ee84d73243154631806a
Shubhra <ssuvro22@outlook.com>
2020-12-15 02:12:21 -0800

    Add Linear and Radial Progress Bar

---
+++ Assets/Editor/ProgressBarEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Editor
+{ 
+    public class ProgressBarEditor : UnityEditor.Editor
+    {
+        [MenuItem("GameObject/UI/CustomUI/Linear Progress Bar")]
+        public static void AddLinearProgressBar()
+        {
+            GameObject bar = Instantiate(Resources.Load<GameObject>("CustomUI/LinearProgressBar"), Selection.activeGameObject.transform, false);
+            bar.transform.name = "LinearProgressBar";
+        }
+        
+        [MenuItem("GameObject/UI/CustomUI/Radial Progress Bar")]
+        public static void AddRadialProgressBar()
+        {
+            GameObject bar = Instantiate(Resources.Load<GameObject>("CustomUI/RadialProgressBar"), Selection.activeGameObject.transform, false);
+            bar.transform.name = "RadialProgressBar";
+        }
+    }
+}
---
+++ Assets/Scripts/ProgressBar/ProgressBar.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace JoystickLab
+{
+    [ExecuteInEditMode]
+    public class ProgressBar : MonoBehaviour
+    {
+        public float minValue;
+        public float maxValue;
+        public float value;
+        [Range(1,10)]public float step;
+        public Image mask;
+        public Image fillImage;
+        public Color fillColor;
+    
+        public float Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                UpdateProgressBar();
+            }
+        }
+        // Update is called once per frame
+        void Update()
+        {
+#if UNITY_EDITOR
+            UpdateProgressBar();
+#endif
+        
+        }
+
+        void UpdateProgressBar()
+        {
+            float maxOffset = maxValue - minValue;
+            float currOffset = Value - minValue;
+            float fill = step * (currOffset / maxOffset);
+            mask.fillAmount = fill;
+            fillImage.color = fillColor;
+        }
+    }
+}
